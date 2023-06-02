70317ca290bbaa8310910021a6c05499591ff697
Macbook <macbook@Macbooks-MacBook-Air-2.local>
2023-06-02 15:31:50 +0530

    issue reproduce

--- app/src/main/java/com/adbanao/MainActivity.kt
+++ app/src/main/java/com/adbanao/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.adbanao
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var centerTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val centerTextView = findViewById<TextView>(R.id.centerTextView)
+        centerTextView = findViewById<TextView>(R.id.centerTextView)
         if(intent.data != null) {
             Firebase.dynamicLinks
-                .getDynamicLink(intent)
+                .getDynamicLink(intent.data!!)
                 .addOnSuccessListener(this) { pendingDynamicLinkData ->
                     if (pendingDynamicLinkData != null) {
                         val deepLink = pendingDynamicLinkData.link
@@ -24,5 +29,23 @@ class MainActivity : AppCompatActivity() {
                 }
         }else
             centerTextView.text = "intent data is null"
+
+
+        //try to get the value of some other link
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            getTheValueOfSecondLink()
+        }
+    }
+
+    private fun getTheValueOfSecondLink(){
+        Firebase.dynamicLinks
+            .getDynamicLink(Uri.parse("https://adbanao.page.link/lottie_video"))
+            .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                if (pendingDynamicLinkData != null) {
+                    val deepLink = pendingDynamicLinkData.link
+                    centerTextView.text = deepLink?.toString()
+                }
+            }
     }
 }
